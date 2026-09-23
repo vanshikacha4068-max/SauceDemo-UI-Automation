1e1eb3d613b84cef51b1df0ab8381e599d990d28
Sushant3761 <singh.sushant3761@gamil.com>
2026-09-23 16:11:16 +0000

    Complete TC01 product image test

--- src/Products/TC01_ProductImage.java
+++ src/Products/TC01_ProductImage.java
@@ -1,61 +1,108 @@
 package Products;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 import java.time.Duration;
+import java.util.List;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
 public class TC01_ProductImage {
 
-	public static void main(String[] args) {
-		//Open The Website
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.saucedemo.com/");
-		
-		//Login with User Name and Password
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-		
-		
-		driver.findElement(By.id("login-button")).click();
-		
-		System.out.println("Login completed");
-		System.out.println("Current URL: " + driver.getCurrentUrl());
-		
-		List<WebElement> products = driver.findElements(
-		        By.xpath("//div[@class='inventory_item']")
-		);
+    public static void main(String[] args) {
 
-		System.out.println("Number of products found: " + products.size());
-		
-		
-		//Using X path Find Element
-		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Open the website
+        WebDriver driver = new ChromeDriver();
 
-		
-		
-		for (WebElement product : products) {
+        driver.get("https://www.saucedemo.com/");
 
-		    String productName = product.findElement(
-		            By.className("inventory_item_name")
-		    ).getText();
+        // Login with username and password
+        driver.findElement(By.id("user-name"))
+                .sendKeys("standard_user");
 
-		    WebElement image = product.findElement(By.xpath(".//img"));
+        driver.findElement(By.id("password"))
+                .sendKeys("secret_sauce");
 
-		    System.out.println("Product: " + productName);
-		    System.out.println("Image displayed: " + image.isDisplayed());
-		    System.out.println("-------------------------");
-		}
-		
-		
-	}
+        driver.findElement(By.id("login-button"))
+                .click();
 
+        System.out.println("Login completed");
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+
+        // Wait until products are available
+        WebDriverWait wait = new WebDriverWait(
+                driver,
+                Duration.ofSeconds(10)
+        );
+
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.className("inventory_item")
+                )
+        );
+
+        // Find all products
+        List<WebElement> products = driver.findElements(
+                By.xpath("//div[@class='inventory_item']")
+        );
+
+        System.out.println("Number of products found: " + products.size());
+
+        boolean allImagesDisplayed = true;
+
+        // Check image of every product
+        for (WebElement product : products) {
+
+            String productName = product.findElement(
+                    By.className("inventory_item_name")
+            ).getText();
+
+            WebElement image = product.findElement(
+                    By.xpath(".//img")
+            );
+
+            // Wait for this particular product image
+            wait.until(
+                    ExpectedConditions.visibilityOf(image)
+            );
+
+            boolean imageDisplayed = image.isDisplayed();
+
+            System.out.println("Product: " + productName);
+            System.out.println("Image displayed: " + imageDisplayed);
+            System.out.println("Image width: "
+                    + image.getSize().getWidth());
+            System.out.println("Image height: "
+                    + image.getSize().getHeight());
+            System.out.println("Image src: "
+                    + image.getAttribute("src"));
+            System.out.println("Image alt: "
+                    + image.getAttribute("alt"));
+            System.out.println("-------------------------");
+
+            // If even one image is not displayed,
+            // the complete test should fail
+            if (!imageDisplayed) {
+                allImagesDisplayed = false;
+            }
+        }
+
+        // Overall test result
+        if (allImagesDisplayed) {
+            System.out.println(
+                    "TC01 PASS: All product images are displayed."
+            );
+        } else {
+            System.out.println(
+                    "TC01 FAIL: One or more product images are not displayed."
+            );
+        }
+
+        // Close browser
+        driver.quit();
+    }
 }
